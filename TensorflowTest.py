4576b29b987f74a2ea6c2130cafde8ea22b90666
Connorwb <connorwb87@gmail.com>
2022-01-17 22:08:00 -0500

    making common workspace

--- TensorflowTest.py
+++
@@ -1,6 +0,0 @@
-try:
-    import tensorflow as tf
-    print(tf.reduce_sum(tf.random.normal([1000, 1000])))
-    print("Looks like Tensorflow is working as intended!")
-except BaseException:
-    print("It doesn't seem like Tensorflow is set up correctly.")
